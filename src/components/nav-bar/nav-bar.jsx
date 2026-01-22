d1908ede4129742a8dd33c26d239ac85146c8997
LordCat <noahcatz@outlook.com>
2026-01-22 16:01:37 -0500

    Add editor button to NavBar

--- src/components/nav-bar/nav-bar.jsx
+++ src/components/nav-bar/nav-bar.jsx
@@ -5,9 +5,10 @@ function NavBar () {
     return (
         <MenuBar>
             <Button onClick={() => location.href = '/'}>Home</Button>
+            <Button onClick={() => location.href = '/editor'}>Editor</Button>
             {/* More items will probably be added in the future */}
         </MenuBar>
-    )
+    );
 }
 
 export default NavBar;
